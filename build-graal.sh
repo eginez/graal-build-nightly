ab87d2951fcdb144ca08cce34608ab9102964631
Esteban <eginez@gmail.com>
2019-12-07 11:41:14 -0800

    initial commit

---
+++ build-graal.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+JDK_VERSION=$1
+
+jdk8() {
+    echo "Building GraalVM for JDK 8"
+    wget -q https://github.com/graalvm/openjdk8-jvmci-builder/releases/download/jvmci-19.3-b04/openjdk-8u232-jvmci-19.3-b04-linux-amd64.tar.gz
+    tar zxf openjdk-8u232-jvmci-19.3-b04-linux-amd64.tar.gz
+    export JAVA_HOME=${CI_PROJECT_DIR}/openjdk1.8.0_232-jvmci-19.3-b04
+}
+
+jdk11() {
+    echo "Building GraalVM for JDK 11"
+    wget -q https://github.com/graalvm/labs-openjdk-11/releases/download/jvmci-19.3-b05/labsjdk-ce-11.0.5+10-jvmci-19.3-b05-linux-amd64.tar.gz
+    tar zxf labsjdk-ce-11.0.5+10-jvmci-19.3-b05-linux-amd64.tar.gz
+    export JAVA_HOME=${CI_PROJECT_DIR}/labsjdk-ce-11.0.5-jvmci-19.3-b05
+}
+
+if [ "${JDK_VERSION}" == "jdk8" ]; then
+    jdk8
+elif [ "$JDK_VERSION" == "jdk11" ]; then
+    jdk11
+else
+    echo "Need to provide a valid JDK version: jdk8, jdk11"
+    exit 1
+fi
+
+mkdir graal
+cd graal
+export PATH=$PWD/mx:$PATH
+
+git clone --depth=1 https://github.com/graalvm/mx
+git clone --depth=1 https://github.com/oracle/graal
+
+cd graal/vm
+echo "Git commit: `git rev-parse HEAD`"
+mx clean
+mx --disable-polyglot --disable-libpolyglot --dynamicimports /substratevm --skip-libraries=true build
+
+# Copy Graal SDK to new directory defined as artifact/cache
+echo "Copying Graal SDK to ${CI_PROJECT_DIR}/graal_dist..."
+cp -R $CI_PROJECT_DIR/graal/graal/vm/latest_graalvm_home/ $CI_PROJECT_DIR/graal_dist
+
