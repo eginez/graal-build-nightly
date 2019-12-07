c9176b0ab7fd4ee468afe6b4e6d0321b1974a250
Esteban <eginez@gmail.com>
2019-12-07 13:35:11 -0800

    fixing tar command

--- build-graal.sh
+++ build-graal.sh
@@ -41,7 +41,7 @@ mx build
 # Copy Graal SDK to new directory defined as artifact/cache
 echo "Copying Graal SDK to ${CI_PROJECT_DIR}/graal_dist..."
 mkdir -p  $CI_PROJECT_DIR/graal_dist_$1
-tar cvfz $CI_PROJECT_DIR/graal/graalvm/latest_graalvm_home/ graal_$1_$commit
+tar -cvfz graal_$1_$commit.tar.gz $CI_PROJECT_DIR/graal/graalvm/latest_graalvm_home/ 
 #cp -R $CI_PROJECT_DIR/graal/graal/vm/latest_graalvm_home/ $CI_PROJECT_DIR/graal_dist_$1
 cp graal_$1_$commit $CI_PROJECT_DIR/graal_dist_$1
 
